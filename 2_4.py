a5a62214753765b83e6b1b18268531198d9295dd
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-24 21:38:16 +1000

    Create 2_4.py

---
+++ 2_4.py
@@ -0,0 +1,20 @@
+# Import pandas
+import pandas as pd
+
+# Hard-code data into list of dictionaries
+data = [
+	{'User':'Mark','Followers':234,'Following':145},
+	{'User':'Eric','Followers':129,'Following':138},
+	{'User':'Lisa','Followers':194,'Following':453},
+	{'User':'Achmad','Followers':123,'Following':194},
+]
+
+# Loop through data
+for d in data:
+	# Per iteration print number of followers
+	print(d['Followers'])
+
+# Converting list of dictionaries into dataframe
+data = pd.DataFrame(data)
+# Writing dataframe into CSV file
+data.to_csv('instagram.csv',sep=';',index=False) 
