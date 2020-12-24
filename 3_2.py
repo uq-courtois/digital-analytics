5aaf2784a9c144d5a02534ad3bd658b309119130
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-25 07:29:06 +1000

    Create 3_2.py

---
+++ 3_2.py
@@ -0,0 +1,32 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+# The URL we want to scrape (CHANGED!)
+url = 'http://www.cedriccourtois.be/fanpage3' 
+ 
+#################################################
+#################################################
+### COPY/PASTE THIS BLOCK AS IS
+ 
+# Open URL (i.e., make request) + disguise agent
+headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+req = Request(url, headers=headers)
+context = ssl._create_unverified_context()
+ 
+# To fetch html and store in variable 'html'
+uClient= urlopen(req, context=context)
+html = uClient.read() # html is stored in variable html
+uClient.close()
+ 
+#################################################
+#################################################
+ 
+### Interpret the page source as html
+soup = BeautifulSoup(html, 'html.parser')
+ 
+### Find the hyperlinks (href) in all of these <a>-tags and store them in the variable hyperlink
+ 
+for link in soup.find_all('a'): # Loop through all the <a> tags
+	hyperlink = link['href'] # Specifically get the contents of the href attribute
+	print(hyperlink) # print result of iteration 
