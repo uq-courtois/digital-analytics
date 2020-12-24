ae76ba99ea8daf6bd62f64e7324001e6d06d9283
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-24 18:22:01 +1000

    Update 2_1_inst.py

--- 2_1_inst.py
+++ 2_1_inst.py
@@ -3,7 +3,3 @@
 # Mark    English   A
 # Eric    Math      B
 # Eric    English   B
- 
-# Enter data from table into your Python script
-# i.e., in a hard-coded list of dictionaries
-# Write it into a new csv file ‘grades.csv’
