79a5b61532665168ba5dedf52083c3151f189eca
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-24 21:30:37 +1000

    Create 2_4.py

---
+++ 2_4.py
@@ -0,0 +1,21 @@
+# Import pandas
+import pandas as pd
+ 
+# Reading data from csv
+data = pd.read_csv('http://112.213.36.202/static/files/2_5.csv',sep=';')
+# Converting dataframe into list of dictionaries
+data = data.T.to_dict().values()
+
+# Create empty list to store domains
+domains = []
+ 
+# Iterate through the imported data
+for item in data:
+	# Split on forward slash and keep first index to extract domain
+	# Store in variable domain
+	domain = item['URL'].split('/')[0]
+	# Append variable domain to list domains, iteration per iteration
+	domains.append(domain)
+
+# Convert list to set and back to list to filter duplicates
+print(list(set(domains)))
