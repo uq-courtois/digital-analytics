711ea8c5d2b61e87d81fc4c9be7281402a4c5b7e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-25 06:50:50 +1000

    Create inst3_2.py

---
+++ inst3_2.py
@@ -0,0 +1,58 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+# The URL we want to scrape
+url = 'http://www.cedriccourtois.be/fanpage4' 
+ 
+#################################################
+#################################################
+### COPY/PASTE THIS BLOCK AS IS
+ 
+# Open URL (i.e., make request) + disguise agent
+headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+req = Request(url, headers=headers)
+context = ssl._create_unverified_context()
+ 
+# To fetch html and store in variable 'html'
+uClient= urlopen(req, context=context)
+html = uClient.read() # html is stored in variable html
+uClient.close()
+ 
+#################################################
+#################################################
+ 
+### Interpret the page source as html
+soup = BeautifulSoup(html, 'html.parser')
+
+# Isolate the div with the post links
+linkdiv = soup.find('div',class_='postindex')
+
+# Loop through the list items (li-tags) in this isolated div
+for item in linkdiv.find_all('li'):
+	# Per li access the a-tag and get the contents of the href attribute
+	url = item.find('a')['href']
+	# Add the domain name to the scraped link compile a browser-accessible link
+	url = 'http://www.cedriccourtois.be/fanpage4/'+url
+	print(url)
+
+	# Request information at url location
+	req = Request(url, headers=headers)
+	context = ssl._create_unverified_context()
+	uClient= urlopen(req, context=context)
+	html = uClient.read() # html is stored in variable html
+	uClient.close()
+
+	# Interpret the page source as html
+	souppostpage = BeautifulSoup(html, 'html.parser')
+
+	# Get postdiv
+	blogpost = souppostpage.find('div',class_='blogpost')
+
+	# Print title text
+	print(blogpost.find('h2').getText())
+
+	# Print paragraph
+	print(blogpost.find('p').getText())
+
+	print()
