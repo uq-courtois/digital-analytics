3eb6869535cfcf0fac47ed3af18934d0d32ef754
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-24 18:19:46 +1000

    Create 2_1_inst.py

---
+++ 2_1_inst.py
@@ -0,0 +1,9 @@
+# Name    Couse     Grade
+# Mark    Math      A
+# Mark    English   A
+# Eric    Math      B
+# Eric    English   B
+ 
+# Enter data from table into your Python script
+# i.e., in a hard-coded list of dictionaries
+# Write it into a new csv file ‘grades.csv’
