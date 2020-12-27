a55a2bc4a09da4097cf5b8898e1950d27b2b1ddc
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-12-27 22:08:54 +1000

    Update 4_6.py

--- 4_6.py
+++ 4_6.py
@@ -5,17 +5,19 @@ from urllib.request import Request, urlopen
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer BQBw0yD2iV67wqzgjYXkET17RhZv65BgjidA3j2r_qQRjDE-MFrfqtgAiUfuwBR9krCRrI7VOiIlNQfd2kekI-Jtf5oO09nQhAdOwC0KX9L43RVc7ANcLw6i6sf7qDFvZPeXo4EYCv_20hEHM9XLpqUItShTc9UOmkpxfpJ6FAmqHKNlnjNw65iB7f_AUow0finEpyEKPhebeJGAe84eGPNoAXfLMp-7waoow7Pce7LDQjiadSTcRcXiuXjr7GxNAVEYT-YU0VQ1HWrf4Q',
+    'Authorization': 'Bearer BQAIksA4t4r1JjjsVR0qwYkFq8wDGEtbp8Buo1pTxJrfvl8s8UJ_Qdwi5Jgsncn-efPxI4MuS2G0oaSihRlJKAFDrckn7-Y-rkB8v3hhRoSH1W6VenizbqqPUClXcsrCtsWQQar4Y2vCAfjutkq_AaIPOFh5HlkBN_PekrdSheaws1CszNp2apvaZOORexskxcElLA2F9T9fIJ3t4KbEdFoSSdveyx1Absa_0ozvKlrfFZHXaZXvJD6rCHI_Zjgdd4dV-zr36n7U-ZBerA',
 } # You will need to change the authorization key into an active one, this one will be expired - Get it - when you are logged in to Spotify - from https://developer.spotify.com/console/get-search-item/?q=tania%20bowra&type=artist&market=&limit=&offset= (scroll down and click get Token (DO NOT USE SAFARI AS YOUR BROWSER)
 
 params = (
         ('q', 'Lose Yourself Eminem'),
         ('type', 'track'),
-	('limit', 1)
+				('limit', 1)
     )
 
 response = requests.get('https://api.spotify.com/v1/search', headers=headers, params=params) # request the response
 # The endpoint base url is supplemented by the arguments in the variables headers and params
+
+print(response) # OK if 200
 
 json_data = json.loads(response.text) # convert json response to text/dict
 
