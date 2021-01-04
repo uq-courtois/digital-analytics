43b7975dcef89263fed867fe21ffc6b49f1ae548
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-04 18:32:46 +1000

    Create merging1.py

---
+++ merging1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read file 1
+df1 = pd.read_csv('imdb_data_a.csv',sep=';') 
+# Read file 2
+df2 = pd.read_csv('imdb_data_b.csv',sep=';')
+
+print(df1.info()) # 10 variables, 13969 rows
+print(df2.info()) # Same 10 variables, 19999 rows
+
+df = df1.append(df2)
+
+print(df.info()) # Same 10 variables, 33968 rows
