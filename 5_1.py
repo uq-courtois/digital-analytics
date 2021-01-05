77eec9ba62235b8175b918e102846ecd9ca96950
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-05 10:06:46 +1000

    Update 5_1.py

--- 5_1.py
+++ 5_1.py
@@ -15,4 +15,4 @@ print(df1.shape) # Shape of first df, 2000 rows, 6 variables
 print(df2.shape) # Shape of second df, 2999 rows, 6 variables
 print(df.shape) # Shape of integrated df, 4999 rows, 6 variables
 
-df.to_csv('mtv-integrated.csv',sep=';',index=False) # Save file
+df.to_csv('mtv-integrated.csv',sep=',',index=False) # Save file
