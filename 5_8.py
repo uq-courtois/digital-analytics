8715fa1bebc160d8bf88ae28d0a0500ef3e73234
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-05 12:11:51 +1000

    Create 5_8.py

---
+++ 5_8.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Read file
+df = pd.read_csv('youtube_vevo.csv',sep=';') 
+
+# Print df info to get a variable overview
+print(df.info())
+
+# Explore the published variable
+print(df['published'])
+
+# We need the four last characters of published in our new variable
+df['year'] = df['published'].str[-4:]
+
+# Convert from object (string) to integer
+# We need this if we want to treat it as a number later on
+df['year'] = df['year'].astype(int) 
+
+# Check the year variable
+print(df['year'])
