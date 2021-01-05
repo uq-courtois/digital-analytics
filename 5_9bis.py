c728104e9784f544b415ce7e571d94a582ed9274
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-05 15:14:56 +1000

    Create 5_9bis.py

---
+++ 5_9bis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+ 
+# Read file
+df = pd.read_csv('youtube_vevo.csv',sep=';') 
+ 
+# Print df info to get a variable overview
+print(df.info())
+ 
+# We need the four last characters of published in our new variable
+df['year'] = df['published'].str[-4:]
+df['year'] = df['year'].astype(int)
+ 
+ 
+def yearcat(x):
+	if x >= 2005 and x < 2010:
+		y = '2005 until 2010'
+	if x >= 2010 and x < 2015:
+		y = '2010 until 2015'		
+	if x >= 2015:
+		y = '2015 until now'
+	return y
+ 
+df['yearcat'] = df['year'].apply(lambda x: yearcat(x))
