42b59fbd2024610d7f15df4836837ee81baa55ab
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-05 15:05:05 +1000

    Create 5_16.py

---
+++ 5_16.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+
+# Read file
+df = pd.read_csv('youtube_vevo.csv',sep=';') 
+ 
+### MEAN, STANDARD DEVIATION (std), MEDIAN (50%)
+print(df['like'].describe())
+ 
+### HISTOGRAM
+# Render histogram (By increasing the bins number, you smoothen the graph)
+plt.hist(df['like'], bins=100)
+
+# Tidying up plot
+plt.ticklabel_format(style='plain')
+plt.xticks(rotation='vertical')
+plt.tight_layout()
+
+# Save figure as pdf
+plt.savefig('histo_duration.pdf')
+ 
+plt.clf() # Clear figure
