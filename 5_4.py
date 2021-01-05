d49976fa4b2b80e1bae49f023b6e3294716e6f8e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-05 10:53:32 +1000

    Update 5_4.py

--- 5_4.py
+++ 5_4.py
@@ -8,3 +8,14 @@ df = df.sort_values(by=['views'], ascending=False)
 
 # Print first 25 values of titles and views
 print(df[['title','views']].head(25))
+
+# Added for your information - not part of the exercise
+# Suppose you want to include this as a table in a report...
+# Copy/pasting from the console is a terrible mess
+# We can save this info as a file and load it into MS Excel,
+# format it, and then copy/paste it to MS Word
+
+table = df[['title','views']].head(25)
+table.to_csv("table.csv",index=False)
+
+# Python makes life easier...
