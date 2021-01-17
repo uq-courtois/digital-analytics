74d673b1cf1853e422248a7842f3d354c9575942
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-17 15:03:55 +1000

    Create instx5.py

---
+++ instx5.py
@@ -0,0 +1,4 @@
+x[0] # will access the first element of list x
+x[1] # will access the second element of list x
+x[-1] # will access the last element of list x
+x[:2] # will access the first two elements of list x
