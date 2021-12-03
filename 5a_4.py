dd9499d151f6691cbafb9cd7d526d6f9e7587ac8
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-03 17:18:13 +1000

    Create 5a_4.py

---
+++ 5a_4.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Read data
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/duplicates.csv',delimiter=',')
+
+# Print the dataframe contents
+print('\nDF before:\n',df)
+
+# Print number of exact duplicates
+print('\n# Exact duplicates:',df.duplicated().sum()) 
+
+# Print rows with duplicates
+print('\nRows with duplicates:\n',df[df.duplicated()])
+
+# Drop exact duplicates
+df = df.drop_duplicates()
+
+# Print the dataframe contents
+print('\nDF after:\n',df)
