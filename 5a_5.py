723c2bc3699ef11221525355d2f78f3fad50ab9f
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-03 17:22:06 +1000

    Create 5a_5.py

---
+++ 5a_5.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Read data
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/duplicates.csv',delimiter=',')
+
+# Print the dataframe contents
+print('\nDF before:\n',df)
+
+# Print number of local duplicates in variable Artist
+print('\n# Exact duplicates:',df.duplicated(subset=['Artist']).sum()) 
+
+# Print rows with duplicates in variable Artist
+print('\nRows with duplicates:\n',df[df.duplicated(subset=['Artist'])])
+
+# Drop duplicates in variable Artist
+df = df.drop_duplicates(subset=['Artist'])
+
+# Print the dataframe contents
+print('\nDF after:\n',df)
