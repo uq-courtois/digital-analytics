b6f958490fec527cbd9ae99734832550f585957b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-03 18:02:00 +1000

    Create 5a_9.py

---
+++ 5a_9.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Read data
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/dob.csv',delimiter=',')
+
+# DF before
+print('\nBefore',df)
+
+def age_cal(date_of_birth):
+	age = 2021 - date_of_birth
+	return age
+
+df['age'] = df['date_of_birth'].apply(lambda x: age_cal(x))
+
+# The result
+print('\nAfter',df)
