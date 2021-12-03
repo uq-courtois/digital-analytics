338b71abefe070b85a7e93478bc275188c044302
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-03 17:29:27 +1000

    Create 5a_6.py

---
+++ 5a_6.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Read data
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/missing.csv',delimiter=',')
+
+# Print the dataframe contents
+print('\nDF before:\n',df)
+
+# Get summary of variables' missing data count
+print('\n # Missing data:\n',df.isnull().sum())
+ 
+# Print rows with missing data (add variable you want to inspect - here 'Song')
+isolatemissing = pd.isnull(df['Song'])
+print('\n Rows with missing data:\n',df[isolatemissing])
+
+# Drop rows with at least one missing value
+df.dropna()
+
+# Print the dataframe contents
+print('\nDF after:\n',df)
