8f8df438e49118d56a85f96b8264aad380373369
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-06 15:46:30 +1000

    Create scrapetable.py

---
+++ scrapetable.py
@@ -0,0 +1,29 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+def scraper(url):
+ 
+	headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	req = Request(url, headers=headers)
+	context = ssl._create_unverified_context()
+	
+	uClient= urlopen(req, context=context)
+	html = uClient.read()
+	uClient.close()
+ 
+	return BeautifulSoup(html, 'html.parser')
+
+soup = scraper('https://digitalanalytics.id.au/static/materials/beyonce/index.html')
+
+# From here on new code is added to the scraping snippet
+table = soup.find('table')
+
+for row in table.find_all('tr'):
+	try:
+		album = row.find('td',class_='album').getText()
+		sales = row.find('td',class_='sales').getText()
+		release = row.find('td',class_='release').getText()
+		print(album,sales,release)
+	except:
+		pass
