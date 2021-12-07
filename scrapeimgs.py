6023266b57d8aeaa8917b8b8a4c5dbdc7519d4ff
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-07 11:37:22 +1000

    Update scrapeimgs.py

--- scrapeimgs.py
+++ scrapeimgs.py
@@ -19,13 +19,13 @@ soup = scraper('https://digitalanalytics.id.au/static/materials/')
 # Find and loop through all the img elements
 for i in soup.find_all('img'):
 
-	# Get the filename from the img source attribute
-	filename = i['src']
-
-	# Construct img url by concatenating page url with filename
-	imgurl = 'https://digitalanalytics.id.au/static/materials'+filename
+	# Get the img url from the img source attribute
+	imgurl = i['src']
 	print(imgurl)
 	
+	# Isolate file name, using the text right after the last / in the url
+	filename = imgurl.split('/')[-1]
+
 	# Create a new, empty picture file in project
 	imgfile = open(filename,'wb') 
 
