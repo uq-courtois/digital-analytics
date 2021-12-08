98e1439fa477bf1e1772671d203849213a20055e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:30:48 +1000

    Update 5a_7.py

--- 5a_7.py
+++ 5a_7.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 # Read data
-df = pd.read_csv('https://digitalanalytics.id.au/static/files/releaseyear.csv',delimiter=',')
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/releaseyear.csv',sep=',')
 
 # Print the dataframe
 print(df.info())
 print(df)
 
-# Create new variable year
+# Create new variable named year
 # Based on the existing variable release_year
-# Grabs the fifth, fourth, third, and second to last digits
+# Grabs the fifth, fourth, third, and second last digits
 df['year'] = df['release_year'].str[-5:-1]
 
 # Convert variable year to integer
