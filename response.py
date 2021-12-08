90fa508a6863e71504bfa4c8e9c67b0c45aa4313
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 12:17:21 +1000

    Create response.py

---
+++ response.py
@@ -0,0 +1,64 @@
+{
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "280-284",
+               "short_name" : "280-284",
+               "types" : [ "street_number" ]
+            },
+            {
+               "long_name" : "Sir Fred Schonell Drive",
+               "short_name" : "Sir Fred Schonell Dr",
+               "types" : [ "route" ]
+            },
+            {
+               "long_name" : "Saint Lucia",
+               "short_name" : "St Lucia",
+               "types" : [ "locality", "political" ]
+            },
+            {
+               "long_name" : "Brisbane City",
+               "short_name" : "Brisbane",
+               "types" : [ "administrative_area_level_2", "political" ]
+            },
+            {
+               "long_name" : "Queensland",
+               "short_name" : "QLD",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "Australia",
+               "short_name" : "AU",
+               "types" : [ "country", "political" ]
+            },
+            {
+               "long_name" : "4067",
+               "short_name" : "4067",
+               "types" : [ "postal_code" ]
+            }
+         ],
+         "formatted_address" : "280-284 Sir Fred Schonell Dr, St Lucia QLD 4067, Australia",
+         "geometry" : {
+            "location" : {
+               "lat" : -27.4936664,
+               "lng" : 153.0067066
+            },
+            "location_type" : "ROOFTOP",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : -27.4923174197085,
+                  "lng" : 153.0080555802915
+               },
+               "southwest" : {
+                  "lat" : -27.49501538029151,
+                  "lng" : 153.0053576197085
+               }
+            }
+         },
+         "place_id" : "ChIJIV_bMzVRkWsR6pWOygFaVHs",
+         "types" : [ "establishment", "point_of_interest", "university" ]
+      }
+   ],
+   "status" : "OK"
+}
