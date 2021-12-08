9eeda410dcc1b63e25e27584378a0dbabccdfeaa
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 12:55:02 +1000

    Update 2_7.py

--- 2_7.py
+++ 2_7.py
@@ -2,27 +2,26 @@
 import pandas as pd
  
 # Reading data from csv
-data = pd.read_csv('http://digitalanalytics.id.au/static/files/2_6.csv',sep=';')
+data = pd.read_csv('http://digitalanalytics.id.au/static/files/2_6.csv',sep=',')
 # Converting dataframe into list of dictionaries
 data = data.T.to_dict().values() 
 
 # Loop through data
 for d in data:
-	# Replace non-string characters in values of followers and following key values with suitable numerical equivalents. 
-	# K = 1000, so replaced by 000
-	# M = 1000000, so replaced by 000000
-	# Get rid of the , altogether
-	d['followers'] = d['followers'].replace('K','000')
-	d['followers'] = d['followers'].replace('M','000000')
-	d['followers'] = d['followers'].replace(',','')
-	d['following'] = d['following'].replace('K','000')
-	d['following'] = d['following'].replace('M','000000')
-	d['following'] = d['following'].replace(',','')
+    # Replace punctuation marks in string varibales of artist and song_title
+    # Replace the / with a space to separate the words
+    # Replace the ( and ) with nothing to remove them
+    d['song_title'] = d['song_title'].replace('/', ' ')
+    d['song_title'] = d['song_title'].replace('(', '')
+    d['song_title'] = d['song_title'].replace(')', '')
+    d['artist'] = d['artist'].replace('/', ' ')
+    d['artist'] = d['artist'].replace('(', '')
+    d['artist'] = d['artist'].replace(')', '')
 
-	# Print updated version of the dictionary to check
-	print(d)
+    # Print updated values to check
+    print(d['song_title'],'by',d['artist'])
 
 # Converting temporary list of dictionaries into dataframe
-newdata = pd.DataFrame(data) 
+newdata = pd.DataFrame(data)
 # Writing dataframe into CSV file
-newdata.to_csv('instagram_clean.csv',sep=';',index=False) 
+newdata.to_csv('songinfo_clean.csv', sep=',', index=False)
