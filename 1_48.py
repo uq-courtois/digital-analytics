e88839bfe9d7f2d4668d1f297bf044c6915d5af0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:46:45 +1000

    Update 1_48.py

--- 1_48.py
+++ 1_48.py
@@ -1,3 +1,12 @@
 x = ['Lisa', 'Eric', 'Marc']
-for y in x:
-	print(y)
+for i in x:
+	print(i)
+
+# Additional solution:
+
+x = ['Lisa', 'Eric', 'Marc']
+for name in x:
+	print(name)
+	
+# You can choose any variable name here for your iteration variable (i, item, name, y, etc.) 
+# as long as it's unique (i.e., not used elsewhere in your code)
