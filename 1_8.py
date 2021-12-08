97d70e61fc60d0dbfaf61d639ee177a79edc93d6
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:37:39 +1000

    Update 1_8.py

--- 1_8.py
+++ 1_8.py
@@ -1,1 +1,13 @@
 print(2+2)
+
+# Additional solutions:
+x = 2
+print(x+x)
+
+x = 2
+y = 2
+print(x+y)
+
+# Although a shorter solution is preferred, these solutions are also correct as 
+# they just separate the input/output. These is no hardcoding involved because we 
+# can still change the number at the input level and the output would update accordingly.
