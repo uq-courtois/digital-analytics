e20d3266fb62b2098892cdcf9618a4fb63b81641
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:52:03 +1000

    Update 1_53.py

--- 1_53.py
+++ 1_53.py
@@ -1,4 +1,10 @@
 lengths = [3,6,8]
 
-for l in lengths:
-  print('The surface is',l*l,'square meters')
+for length in lengths:
+  print('The surface is',length*length,'square meters')
+
+# Additional solution:
+
+lengths = [3, 6, 8]
+for length in lengths:
+    print('The surface is', length**2, 'square meters')
