419ba6a5c97f62f2a2781b7ec007cf879bd98658
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:42:52 +1000

    Update 1_25.py

--- 1_25.py
+++ 1_25.py
@@ -3,3 +3,9 @@ x = {
 'grade':6,
 'pass':'Yes'
 }
+
+# Note that the key-value pairs don't have to be separated out onto their own lines.
+# This just makes it a little bit easier to read
+
+# You can also write a dictionary like this:
+x = {'name':'Joan','grade':6,'pass':'Yes'}
