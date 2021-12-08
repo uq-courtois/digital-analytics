b0772cab11e79ae3958b258cac89c88bb83582a7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:50:44 +1000

    Update 1_52.py

--- 1_52.py
+++ 1_52.py
@@ -3,3 +3,13 @@ number = 10
 while number/2 >= 1:
 	number = number/2
 	print(number)
+
+# Additional solution:
+
+number = 10
+while number >= 1:
+    print(number)
+    number = number/2
+
+# Again, the order of steps within the while loop can change as long as it 
+# matches the logic of the while loop conditional statement
