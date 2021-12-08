cda36d188a1a5a13f99e94d1edd708914e48bfd0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:40:16 +1000

    Update instx5.py

--- instx5.py
+++ instx5.py
@@ -1,4 +1,5 @@
 x[0] # will access the first element of list x
 x[1] # will access the second element of list x
 x[-1] # will access the last element of list x
-x[:2] # will access the first two elements of list x
+x[:2] # will access the first two elements of list x, the : here means up to but not including the third element [2]
+x[-2:] # will access the last two elements of list x, the : here means from the second last element [-2] until the end
