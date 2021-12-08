8629cfb3ff1e93999e99f9761ae6a2111fe4751f
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 12:23:17 +1000

    Update geocoding.py

--- geocoding.py
+++ geocoding.py
@@ -4,27 +4,29 @@ import json
 from urllib.request import urlopen
 
 # Endpoint URL
-baseurl = "https://maps.googleapis.com/maps/api/geocode/json?" 
+endpoint = "https://maps.googleapis.com/maps/api/geocode/json?" 
 
 # Build query for address argument
-query = "address=" + "University of Queensland St Lucia" 
+query =  'University of Queensland St Lucia'
 # Replace spaces in query with +
 query = query.replace(' ','+') 
 
 # API key - you will need to get your own to try this example
 # The API key in this example is deactivated for security reasons
-apikey = "&key=AIzaSyDC60i9o-E4sOVCsYCZCYGB3DIlhAenZy0" 
+apikey = "AIzaSyDC60i9o-E4sOVCsYCZCYGB3DIlhAenZy0" 
 
-# Combining all the url components into a single url
-compiledurl = baseurl + query + apikey 
+# Combine all the url components into a single url + print url
+compiledurl = endpoint + 'address=' + query + '&key=' + apikey 
+print(compiledurl)
 
 # Send request + get response
-json_object = urlopen(compiledurl) 
+response = urlopen(compiledurl) 
 
 # Convert JSON result
-locationdata = json.load(json_object) 
- 
-print(locationdata)
+data = json.load(response) 
 
-# Use this print below to have the response nicely formatted
-# print(json.dumps(locationdata, sort_keys=True, indent=3)) 
+# Print data variable without formatting:
+print (data) 
+
+# Print data variable with formatting
+# print(json.dumps(data, sort_keys=True, indent=3)) 
