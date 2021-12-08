f173cd77d2500956d4c5ac9250c9e6188ff053b0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:49:50 +1000

    Update 1_51.py

--- 1_51.py
+++ 1_51.py
@@ -3,3 +3,20 @@ number = 0
 while number < 10:
   number = number + 1
   print(number)
+
+# Additional solution:
+
+number = 1
+while number < 11:
+    print(number)
+    number = number + 1
+
+# You can print the number first, or add one to the number first, it doesn't matter-
+# as long as the logic in your while loop conditional matches the order you are doing the steps within it
+
+# For example, the follow solution would be incorrect:
+number = 1
+while number < 10:
+    number = number + 1
+    print(number)
+# As this would give us a program that counts from 2-11
