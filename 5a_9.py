ad935c6b229a8eed282f8a29392489ef0a24358c
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 11:32:04 +1000

    Update 5a_9.py

--- 5a_9.py
+++ 5a_9.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Read data
-df = pd.read_csv('https://digitalanalytics.id.au/static/files/dob.csv',delimiter=',')
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/dob.csv',sep=',')
 
 # DF before
 print('\nBefore',df)
 
 def age_cal(date_of_birth):
-	age = 2021 - date_of_birth
+	age = 2022 - date_of_birth
 	return age
 
 df['age'] = df['date_of_birth'].apply(lambda x: age_cal(x))
