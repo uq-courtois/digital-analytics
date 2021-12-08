51d009f7faf800de28b0b8527997c534e104b8a3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 13:06:10 +1000

    Update response2.py

--- response2.py
+++ response2.py
@@ -1,7 +1,7 @@
 import json
 from urllib.request import urlopen
 
-# To make the code shorter in this snipper, the request url is hardcoded
+# To make the code shorter in this snippet, the request url is hardcoded
 requesturl = 'https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Queensland+St+Lucia&key=AIzaSyB_kohrkvQDmgFaqHIOcJkFNCnolK7xRq4'
 
 # Send request + get response
