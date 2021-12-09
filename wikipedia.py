f8669edacf208057e408f603cca64eccd9b9c51f
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-09 13:54:24 +1000

    Create wikipedia.py

---
+++ wikipedia.py
@@ -0,0 +1,21 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+def scraper(url):
+ 
+	headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	req = Request(url, headers=headers)
+	context = ssl._create_unverified_context()
+	
+	uClient= urlopen(req, context=context)
+	html = uClient.read()
+	uClient.close()
+ 
+	return BeautifulSoup(html, 'html.parser')
+ 
+soup = scraper('https://en.wikipedia.org/wiki/Vin_Diesel')
+
+# Isolate div with class name and extract text
+text = soup.find('div',class_='mw-parser-output').getText()
+print(text)
