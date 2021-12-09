0a88f507e00e3f012e57e8b7e58cdd7225017aa7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-09 11:03:10 +1000

    Create scrape_ex4.py

---
+++ scrape_ex4.py
@@ -0,0 +1,36 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+def scraper(url):
+ 
+	headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	req = Request(url, headers=headers)
+	context = ssl._create_unverified_context()
+	
+	uClient= urlopen(req, context=context)
+	html = uClient.read()
+	uClient.close()
+ 
+	return BeautifulSoup(html, 'html.parser')
+ 
+soup = scraper('https://digitalanalytics.id.au/static/materials/mockup')
+
+# Find and loop through all the img elements
+for i in soup.find_all('img'):
+ 
+	# Get the img url from the img source attribute
+	imgurl = i['src']
+	print(imgurl)
+	
+	# Isolate file name, using the text right after the last / in the url
+	filename = imgurl.split('/')[-1]
+ 
+	# Create a new, empty picture file in project
+	imgfile = open(filename,'wb') 
+ 
+	# Write picture information into empty file
+	imgfile.write(urlopen(imgurl).read())
+ 
+	# Close file 
+	imgfile.close() 
