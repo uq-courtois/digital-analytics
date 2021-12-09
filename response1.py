75a4fe05e41bc03f39bc32b372340092e1fa3e01
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-09 10:38:57 +1000

    Update response1.py

--- response1.py
+++ response1.py
@@ -2,6 +2,7 @@ import json
 from urllib.request import urlopen
 
 # To make the code shorter in this snippet, the request url is hardcoded
+# Remember this code snippet won't work unless you replace the API key with a valid new key
 requesturl = 'https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Queensland+St+Lucia&key=AIzaSyB_kohrkvQDmgFaqHIOcJkFNCnolK7xRq4'
 
 # Send request + get response
