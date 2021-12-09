6ad8edba0db7846518e009c17f021144bc447dbf
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-09 10:49:06 +1000

    Create scrape_ex1.py

---
+++ scrape_ex1.py
@@ -0,0 +1,20 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+ 
+def scraper(url):
+ 
+	headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	req = Request(url, headers=headers)
+	context = ssl._create_unverified_context()
+	
+	uClient= urlopen(req, context=context)
+	html = uClient.read()
+	uClient.close()
+ 
+	return BeautifulSoup(html, 'html.parser')
+ 
+soup = scraper('https://digitalanalytics.id.au/static/materials/mockup')
+
+title = soup.find('h1').getText()
+print(title)
