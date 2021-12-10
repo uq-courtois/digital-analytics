465b903623749174b9d9e9912f6152f7e3791309
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-10 15:49:16 +1000

    Create api_ex_7.py

---
+++ api_ex_7.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import pandas as pd
+
+# Reading data from csv
+data = pd.read_csv('http://112.213.36.202/static/files/4_1.csv',sep=';') 
+# Converting dataframe into list of dictionaries
+data = data.T.to_dict().values() 
+
+# Use your own token - this one has expired
+token = 'BQB_L23a4yd4rH_6Wb2rDNccymwTM5YG-Inn3CJfWGOAlabNx3_VzFyGVO-oM6RgAG3VXa9mfdCWCe7aOkcIQ4ns9TMPKV0Sq0dp4SxKeJS4I3UrJXvctCjY4mnGoC2_Fb9lenQdBUvSQK5sKfjyZHakZ_yBAXXvdN3s7Rv5ozGDfZjGMpyZMUAFR8Mf8siuyD2i1F1vKTcwrRT9s3mud0e4bpH1gnOhzadc7CZoaRBUZbcEFnymNwgjLr8FrqYm8DqSjFMNV6POtiCNbvL8djg'
+ 
+# Iterate through the imported data
+for item in data:
+	print('Searching for',item['Artist'])
+ 
+	headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token,
+	} 
+ 
+	params = (
+					('q', item['Artist']), # Per iteration, the value of q = the read artist
+					('type', 'artist'),
+			)
+ 
+	response = requests.get('https://api.spotify.com/v1/search',headers=headers, params=params) # request the response
+	# The endpoint base url is supplemented by the arguments in the variables headers and params
+ 
+	print(response) # OK when 200
+ 
+	json_data = json.loads(response.text) # convert json response to text/dict
+ 
+	genres = json_data['artists']['items'][0]['genres'] # Isolating the genre information - as a list
+
+	print(item['Artist'],genres)
