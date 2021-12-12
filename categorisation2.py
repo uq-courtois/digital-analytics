7169f85ce1845a0c3e319d2b2376e53c7bc018cc
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-12 20:56:10 +1000

    Create categorisation2.py

---
+++ categorisation2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+ 
+# Read data
+df = pd.read_csv('https://digitalanalytics.id.au/static/files/youtube_vevo.csv',sep=',')
+
+# Function to categorise: receives title, returns lyric_video
+def lyric_video(title):
+	if 'lyric video' in title.lower():
+		print(title)
+	else:
+		lyric_video = 'no'
+	return lyric_video
+
+df['lyric_video'] = df['title'].apply(lambda x: lyric_video(x))
+
+print(df['view_cat'])
