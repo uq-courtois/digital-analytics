4b4b3edb6c9f48bc7013946d853fcf528922f6ca
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-12 20:54:39 +1000

    Update publishedtoyear.py

--- publishedtoyear.py
+++ publishedtoyear.py
@@ -3,6 +3,6 @@ import pandas as pd
 # Read data
 df = pd.read_csv('https://digitalanalytics.id.au/static/files/youtube_vevo.csv',sep=',')
 
-# Create variable year from published
+# Create variable year from published (extract the last four characters)
 df['year'] = df['published'].str[-4:]
 df['year'] = df['year'].astype('int')
