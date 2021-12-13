7df5b3ca7e1e3c0370c8cd516706ddcd2bf84007
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 14:29:27 +1000

    Update api_ex_6.py

--- api_ex_6.py
+++ api_ex_6.py
@@ -1,35 +1,56 @@
 import json
 import requests
- 
-# Use your own token - this one has expired
-token = 'BQB_L23a4yd4rH_6Wb2rDNccymwTM5YG-Inn3CJfWGOAlabNx3_VzFyGVO-oM6RgAG3VXa9mfdCWCe7aOkcIQ4ns9TMPKV0Sq0dp4SxKeJS4I3UrJXvctCjY4mnGoC2_Fb9lenQdBUvSQK5sKfjyZHakZ_yBAXXvdN3s7Rv5ozGDfZjGMpyZMUAFR8Mf8siuyD2i1F1vKTcwrRT9s3mud0e4bpH1gnOhzadc7CZoaRBUZbcEFnymNwgjLr8FrqYm8DqSjFMNV6POtiCNbvL8djg'
- 
+
+# You will need to change the authorization key into an active one, this one will be expired - Get it - when you are logged in to Spotify - from https://developer.spotify.com/console/get-search-item/ (scroll down and click get Token (DO NOT USE SAFARI AS YOUR BROWSER)
+token = 'BQAn9b3t_lktBWrcYUVJUzwKkt9vzSp4ThKIXIP1jelMtumhBofoH_gRhXxbJKXYPruPJoI9BQbqtOw-aXdpn7DmlcO-huZ8vonkkI5ySVOqynmmhInTFhONwa4yotEHUa5BJZI11haR6ESn29i3g-4R4paeF9tCFnU'
+
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + token,
 } 
- 
-# Add in the trackid for Rihanna's 'We found love'
-trackid = "0U10zFw4GlBacOy9VDGfGL"
- 
-# Setting parameters
+
+# Set parameters
 params = (
-    ('seed_tracks', trackid),
-    ('limit', '10'), # Get the first ten recommendations
-)
- 
-# Making request
-response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params) 
- 
+        ('q', 'Eminem Lose Yourself'),
+        ('type', 'track'),
+	('limit', '1'),
+    )
+
+# Make request
+response = requests.get('https://api.spotify.com/v1/search', headers=headers, params=params) # 
+
 # We want code [200] here, points to a successful request
 print(response) 
- 
+
 # Convert response to text/dict
 data = json.loads(response.text) 
- 
-# Print recommended artst + track names of the first 10 recommendations
-for i in data['tracks']: # Loop through the list with recommendation items , which is the value of data['tracks']
-		print(i['album']['artists'][0]['name'])
-		print(i['name'])
-		print()
+
+# Isolating the required information
+trackid = data['tracks']['items'][0]['id']
+print(trackid)
+print() # Prints a blank line
+
+# Now use that trackid to get recommendations from Spotify
+#The token, headers, and imports can all stay the same as above
+
+# Set new parameters, for recommendations endpoint
+params = (
+    ('seed_tracks', trackid),
+    ('limit', '10'), # Get the first 10
+)
+
+# Make request for recommendations
+response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params) 
+
+# We want code [200] here, points to a successful request
+print(response) 
+
+# Convert response to text/dict
+data = json.loads(response.text) 
+
+# Loop through the tracks and print the artist and track name for each recommendation
+for track in data['tracks']:
+ artist_name = track['album']['artists'][0]['name']
+ track_name = track['name']
+ print(artist_name + ': ' + track_name)
