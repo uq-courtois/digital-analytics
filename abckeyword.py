a7e17438e70087503391787aa0f97daad92edeaa
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 18:44:39 +1000

    Update abckeyword.py

--- abckeyword.py
+++ abckeyword.py
@@ -1,33 +1,49 @@
 import pandas as pd
 import string
-from collections import Counter,OrderedDict
 import nltk
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize 
-
+ 
 # Get stopwords
 sw = pd.read_csv('https://www.digitalanalytics.id.au/static/files/stopwords.csv',sep=',')
 stopwords = sw['stopwords'].tolist()
-
+ 
 # Read data
 df = pd.read_csv('https://www.digitalanalytics.id.au/static/files/abc.csv',sep=',')
  
-# Combine all values of 'text' (i.e., column in the dataframe) into a single string variable 'text'
+# Combine all values of 'headline' into a single string variable 'text'
 text = ' '.join(df['text'].tolist()).lower()
  
 # Remove punctuation from string variable 'text'
 text = text.translate(str.maketrans('', '', string.punctuation))
  
 # Tokenise text (i.e., split up in list of words)
-text_tokens = word_tokenize(text)
+text_tokens = nltk.word_tokenize(text)
  
 # Remove stopwords from the tokenised text
 wordlist = [word for word in text_tokens if not word in stopwords]
  
-# Generate dataframe with extracted keywords, sorted for frequency
-df = pd.DataFrame(list(dict(Counter(wordlist)).items()),columns = ['Word','Frequency']).sort_values(['Frequency'],ascending=False)
- 
+# Extract ngrams into a list of dictionaries (use 2 for bigrams)
+ngrams = []
+for ngram in nltk.ngrams(wordlist, 2):
+	ngrams.append({'Ngram':' '.join(ngram)})
+
+# Get rid of duplicates ngrams
+ngrams = [dict(t) for t in {tuple(d.items()) for d in ngrams}]
+
+# Count and add frequencies to list of dicts
+for ngram in ngrams:
+	print(ngram['Ngram'])
+	ngram['Frequency'] = text.count(ngram['Ngram'])
+	
+# Generate dataframe with extracted ngrams, sorted for frequency
+df = pd.DataFrame(ngrams).sort_values(['Frequency'],ascending=False)
+	
+# Only keep ngrams with a frequency higher than 1
+df = df [df['Frequency'] > 1]
+	
 # Print df
-print(df[['Word','Frequency']].head(50))
- 
+print(df[['Ngram','Frequency']].head(50))
+	
 # Save to CSV file
 df.to_csv('results.csv',sep=',',index=False)
