5cb66bc5bf12230061b2290c192474df2574de26
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:42:04 +1000

    Create corr.py

---
+++ corr.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+ 
+df = pd.read_csv('https://www.digitalanalytics.id.au/static/files/youtube_vevo_clean.csv', delimiter = ',') 
+
+### CORRELATION
+print(rp.correlation.corr_pair(df[['views', 'likes']]))
+ 
+# The r-value is the magnitude of the correlation
+# p-value expresses statistical significance level
+# N reports number of cases the analysis is based on
+ 
+### SCATTERPLOT
+plt.scatter(df['views'],df['likes']) # Build plot
+plt.xlabel('Number of views') # x-axis label
+plt.ylabel('Number of likes') # y-axis label
+plt.tight_layout() # Forces tidy plot lay-out
+plt.savefig('scatterplot.pdf') # Save figure
+plt.clf() # Clear figure
