5f848bbd9886a883bf9d53e357efa7398b67a9e5
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:38:50 +1000

    Create catex.py

---
+++ catex.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+ 
+df = pd.read_csv('https://www.digitalanalytics.id.au/static/files/youtube_vevo_clean.csv', delimiter = ',') 
+ 
+# Set max of rows to show, in/decrease to needs
+pd.set_option('max_rows', 9999) 
+ 
+### MODE, FREQUENCIES, AND COUNTS
+catsum = rp.summary_cat(df['view_cat'])
+print(catsum)
+# Mode is value with highest count: 'USA'
+# Counts are absolute frequencies
+# Percentages are relative frequencies
+ 
+### BART CHART
+plt.bar(catsum['Outcome'],catsum['Count'])
+# x-labels based on outcome strings of catsum
+# bar height based on count figures of catsum
+ 
+# Saving the image to a file
+plt.tight_layout()
+# Saving the image to a file
+plt.savefig('view_cat-absolute-barchart.pdf')
+# Clear figure
+plt.clf() 
+ 
+### BART CHART
+plt.bar(catsum['Outcome'],catsum['Percent'])
+# x-labels based on outcome strings of catsum
+# bar height based on percentage figures of catsum
+ 
+# Forces tidy plot lay-out
+plt.tight_layout()
+# Saving the image to a file
+plt.savefig('view_cat-relative-barchart.pdf')
+# Clear figure
+plt.clf() 
