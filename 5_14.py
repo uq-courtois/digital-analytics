b86ee75940acd1e1f327602ebe741e613e6f3b5b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-14 16:09:04 +1000

    Update 5_14.py

--- 5_14.py
+++ 5_14.py
@@ -1,5 +1,6 @@
 import pandas as pd
- 
+pd.set_option('max_rows', 9999) # To make sure we get all rows
+
 # Read file
 df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/youtube_vevo_clean.csv',sep=',') 
  
