3e36365932cc7bcadb745b5ee08a37f0f66458f3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-14 16:07:27 +1000

    Create aggexercise.py

---
+++ aggexercise.py
@@ -0,0 +1,11 @@
+import pandas as pd
+pd.set_option('max_rows', 9999) # To make sure we get all rows
+
+# Read file
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/youtube_vevo_clean.csv',sep=',') 
+ 
+# Calculate the median views per artist
+grouped = df.groupby(['artist'], as_index=False)[['lyric_video_num']].sum()
+
+# Print artist and views variables
+print(grouped[['artist','lyric_video_num']])
