7c69fd42b4fa1f96b739b3eb5feca5851da859cf
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-15 15:26:23 +1000

    Update mps4.py

--- mps4.py
+++ mps4.py
@@ -6,8 +6,13 @@ import pandas as pd # Added to be able to write a csv file
 # An empty list to store the results
 data = []
 
-for i in range(1,248):
-	url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2021-01-01,2022-01-01&start='+str(i*50)+'&ref_=adv_nxt'
+startnumbers = [1]
+
+for i in range(1,247):
+	startnumbers.append(i*50)
+
+for sn in startnumbers:
+	url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2021-01-01,2022-01-01&start='+str(sn)+'&ref_=adv_nxt'
 	print(url)
 
 	def scraper(url):
