2b9c91e0841f061801698c847e4a9ffeb0a294c2
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-15 15:21:57 +1000

    Update mps2.py

--- mps2.py
+++ mps2.py
@@ -1,3 +1,8 @@
-for i in range(1,248):
-	url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2021-01-01,2022-01-01&start='+str(i*50)+'&ref_=adv_nxt'
+startnumbers = [1]
+
+for i in range(1,247):
+	startnumbers.append(i*50)
+
+for sn in startnumbers:
+	url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2021-01-01,2022-01-01&start='+str(sn)+'&ref_=adv_nxt'
 	print(url)
