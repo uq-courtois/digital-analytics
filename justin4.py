8e7f97773b8fe313bf74032f1fd76164399810a0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-15 15:38:32 +1000

    Update justin4.py

--- justin4.py
+++ justin4.py
@@ -28,20 +28,20 @@ divofinterest = soup.find('div',class_='YAJzu _26IxR _2QLX-')
 # Step 2: Loop though divofinterest and look for all divs with class name _2wWGp-
 for d in divofinterest.find_all('div',class_='_2wWGp'):
 	
-		# Step 3: Narrow down to the individual pieces of information in each div
-		title = d.find('a',class_='_3T9Id fmhNa nsZdE _2c2Zy _1tOey _3EOTW').getText()
-		url = d.find('a',class_='_3T9Id fmhNa nsZdE _2c2Zy _1tOey _3EOTW')['href']
-		description =d.find('div',class_='_1yL-m rMkro _1cBaI _3PhF6 _10YQT').getText()
+	# Step 3: Narrow down to the individual pieces of information in each div
+	title = d.find('a',class_='_3T9Id fmhNa nsZdE _2c2Zy _1tOey _3EOTW').getText()
+	url = d.find('a',class_='_3T9Id fmhNa nsZdE _2c2Zy _1tOey _3EOTW')['href']
+	description =d.find('div',class_='_1yL-m rMkro _1cBaI _3PhF6 _10YQT').getText()
 
-		# Step 4: Append information in a dictionary to list data
+	# Step 4: Append information in a dictionary to list data
 
-		entry = {
-			'title':title,
-			'url':url,
-			'description':description,
-		}
+	entry = {
+		'title':title,
+		'url':url,
+		'description':description,
+	}
 
-		data.append(entry)
+	data.append(entry)
 
 # Step 4: Write into CSV
 # Make sure to import pandas, otherwise this will throw an error
