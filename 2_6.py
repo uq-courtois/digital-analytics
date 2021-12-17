7a458f5816ed8fc8753d543a3834f98bde8a4819
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 15:43:13 +1000

    Update 2_6.py

--- 2_6.py
+++ 2_6.py
@@ -3,6 +3,7 @@ data = pd.read_csv('https://www.digitalanalytics.id.au/static/files/2_6.csv',sep
 data = data.T.to_dict().values()
 
 for i in data:
+  # Add new key in the dictionary in i, multiplying the length and width values
   i['surface'] = i['length'] * i['width']
 
 newdata = pd.DataFrame(data)
