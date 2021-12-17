944caeb7d31d0537550acf3611f56e593f4d80f4
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 16:22:06 +1000

    Update 2_6.py

--- 2_6.py
+++ 2_6.py
@@ -3,5 +3,5 @@ data = pd.read_csv('https://www.digitalanalytics.id.au/static/files/2_5.csv',sep
 data = data.T.to_dict().values()
 
 for i in data:
-  if i['Rank'] >= 3:
+  if i['Rank'] <= 3:
     print(i)
