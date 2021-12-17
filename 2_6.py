da08072bb5eb030f97e4ce455ff2ee8093bf1d83
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 16:21:56 +1000

    Update 2_6.py

--- 2_6.py
+++ 2_6.py
@@ -1,10 +1,7 @@
 import pandas as pd
-data = pd.read_csv('https://www.digitalanalytics.id.au/static/files/2_6.csv',sep=',')
+data = pd.read_csv('https://www.digitalanalytics.id.au/static/files/2_5.csv',sep=';')
 data = data.T.to_dict().values()
 
 for i in data:
-  # Add new key in the dictionary in i, multiplying the length and width values
-  i['surface'] = i['length'] * i['width']
-
-newdata = pd.DataFrame(data)
-newdata.to_csv('rectangles.csv',sep=',',index=False)
+  if i['Rank'] >= 3:
+    print(i)
