e473d7af932f93aa605381d39236371ed52a0130
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 13:47:21 +1000

    Update 63_1.py

--- 63_1.py
+++ 63_1.py
@@ -1,5 +1,5 @@
-# IMPORT PANDAS
 import pandas as pd
-# READING FROM URL (Replace the url below with the url of the file you want to access)
 data = pd.read_csv('https://digitalanalytics.id.au/static/files/csvtutorial.csv',sep=',')
-data = data.T.to_dict().values()  # Converting dataframe into list of dictionaries
+data = data.T.to_dict().values()
+
+print(data)
