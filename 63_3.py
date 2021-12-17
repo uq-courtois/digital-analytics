c6a315aaff8a9a8fb87299bf552ffa4dac2a8df7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 14:03:07 +1000

    Update 63_3.py

--- 63_3.py
+++ 63_3.py
@@ -1,5 +1,3 @@
-# IMPORT PANDAS
 import pandas as pd
-# READING FROM FILE NAME (Replace the name below with the name of the file you want to access)
 data = pd.read_csv('csvtutorial.csv',sep=',')
-data = data.T.to_dict().values()  # Converting dataframe into list of dictionaries
+data = data.T.to_dict().values()
