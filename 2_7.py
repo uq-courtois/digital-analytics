b1d666761eff71887beb3f58fbb0042b2eee2610
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-17 16:18:01 +1000

    Update 2_7.py

--- 2_7.py
+++ 2_7.py
@@ -1,27 +1,10 @@
-# Import pandas
 import pandas as pd
+data = pd.read_csv('https://www.digitalanalytics.id.au/static/files/2_6.csv',sep=',')
+data = data.T.to_dict().values()
  
-# Reading data from csv
-data = pd.read_csv('http://digitalanalytics.id.au/static/files/2_6.csv',sep=',')
-# Converting dataframe into list of dictionaries
-data = data.T.to_dict().values() 
-
-# Loop through data
-for d in data:
-    # Replace punctuation marks in string varibales of artist and song_title
-    # Replace the / with a space to separate the words
-    # Replace the ( and ) with nothing to remove them
-    d['song_title'] = d['song_title'].replace('/', ' ')
-    d['song_title'] = d['song_title'].replace('(', '')
-    d['song_title'] = d['song_title'].replace(')', '')
-    d['artist'] = d['artist'].replace('/', ' ')
-    d['artist'] = d['artist'].replace('(', '')
-    d['artist'] = d['artist'].replace(')', '')
-
-    # Print updated values to check
-    print(d['song_title'],'by',d['artist'])
-
-# Converting temporary list of dictionaries into dataframe
+for i in data:
+  # Add new key in the dictionary in i, multiplying the length and width values
+  i['surface'] = i['length'] * i['width']
+ 
 newdata = pd.DataFrame(data)
-# Writing dataframe into CSV file
-newdata.to_csv('songinfo_clean.csv', sep=',', index=False)
+newdata.to_csv('rectangles.csv',sep=',',index=False)
