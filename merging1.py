c3c0ef83944c377fe62524d479693fa7d98fa3cf
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 16:04:47 +1000

    Update merging1.py

--- merging1.py
+++ merging1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Read file 1
-df1 = pd.read_csv('imdb_data_a.csv',sep=';') 
+df1 = pd.read_csv('imdb_data_a.csv',sep=',') 
 # Read file 2
-df2 = pd.read_csv('imdb_data_b.csv',sep=';')
+df2 = pd.read_csv('imdb_data_b.csv',sep=',')
 
 print(df1.info()) # 10 variables, 13969 rows
 print(df2.info()) # Same 10 variables, 19999 rows
