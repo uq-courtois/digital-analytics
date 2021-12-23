daa20b1a889c3cba01896593b2c64378f3eda121
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-23 16:40:20 +1000

    Update ttestex.py

--- ttestex.py
+++ ttestex.py
@@ -2,14 +2,19 @@ import pandas as pd
 import researchpy as rp
 import matplotlib.pyplot as plt
 from scipy import stats
-
+ 
 df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/youtube_vevo_clean.csv', delimiter = ',') 
-
+ 
 ### BAR PLOTS OF TWO GROUPS FOR A VARIABLE
-
+ 
 # Subset two groups
 group1 = df[df['artist']=='ArianaGrandeVevo']['views']
 group2 = df[df['artist']=='TaylorSwiftVEVO']['views']
+print('Group means:',round(group1.mean(),2),round(group2.mean(),2))
+print('Group stds:',round(group1.std(),2),round(group2.std(),2))
+
+degreesoffreedom = (len(group1)+len(group2)-2)
+print('Degrees of freedom',degreesoffreedom)
 
 means = (group1.mean(),group2.mean()) # Calculating means
 positions = [0, 1] #Defining positions in the graph
@@ -19,6 +24,6 @@ plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False) # Prevent sc
 plt.tight_layout() # Forces tidy plot lay-out
 plt.savefig("barmeanstd.pdf") # Save figure
 plt.clf() # Clear figure
-
+ 
 # Calculate and print Welch’s t-test
 print(stats.ttest_ind(group1, group2, equal_var = False))
