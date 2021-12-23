20992a17a2ab759f6ed14a56f34e60fd260c6f7b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-23 20:42:34 +1000

    Update 5a_2.py

--- 5a_2.py
+++ 5a_2.py
@@ -5,16 +5,15 @@ pd.set_option('max_rows', 9999) # Add this setting to make sure you get to see u
 df = pd.read_csv('https://digitalanalytics.id.au/static/files/artists-spotify.csv',sep=',')
 
 # Get an overall shape of the dataframe
-print(df.shape)
-print() # Print a blank line
+# This is the number of rows and columns
+print(df.shape) 
 
 # Get overall description of the dataframe
 print(df.info())
 
-# Loop through all columns and show unique values:
+# Loop through all columns and count the unique values:
 for c in df.columns.values.tolist():
 	print('\n',len(df[c]),'unique values in variable:',c)
-	print(df[c].unique())
 
 # Get unique values for one particular variable
 # You just need to replace the column name in the ''
