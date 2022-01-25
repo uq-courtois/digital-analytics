f3e3c4399862a3c0efd8344111edadff1b03bf84
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-01-25 16:51:06 +1000

    Update 1_52.py

--- 1_52.py
+++ 1_52.py
@@ -8,7 +8,7 @@ while number/2 >= 1:
 
 number = 10
 while number >= 1:
-    print(number)
+    print(number/2)
     number = number/2
 
 # Again, the order of steps within the while loop can change as long as it 
